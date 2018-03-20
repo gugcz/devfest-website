632a55536d957b7af2dcf9061ccaf690752fc64d
Dominik Šimoník <32575328+ryzizub@users.noreply.github.com>
2018-03-20 16:34:07 +0100

    slower animation

--- src/app/components/organizer-card/organizer-card.component.ts
+++ src/app/components/organizer-card/organizer-card.component.ts
@@ -30,10 +30,10 @@ import {Observable} from 'rxjs/Observable';
       ]), trigger('fadeInOut', [
       transition(':enter', [   // :enter is alias to 'void => *'
         style({ opacity: 0 }),
-        animate('500ms', style({ opacity: 1 }))
+        animate('1500ms', style({ opacity: 1 }))
       ]),
       transition(':leave', [   // :leave is alias to '* => void'
-        animate('500ms', style({ opacity: 0 }))
+        animate('1500ms', style({ opacity: 0 }))
       ])
     ])
   ]
