6726a5e8a5ae8f6231c3963d8b2445b7e8e7e49f
Dominik Šimoník <32575328+ryzizub@users.noreply.github.com>
2018-03-22 10:37:23 +0100

    logo on other pages

--- src/app/components/top-toolbar/top-toolbar.component.ts
+++ src/app/components/top-toolbar/top-toolbar.component.ts
@@ -1,15 +1,37 @@
-import {Component, OnInit} from '@angular/core';
-import {DomSanitizer} from '@angular/platform-browser';
-import {MatIconRegistry} from '@angular/material';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-top-toolbar',
   templateUrl: './top-toolbar.component.html',
-  styleUrls: ['./top-toolbar.component.scss']
+  styleUrls: ['./top-toolbar.component.scss'],
+  animations: [trigger('fadeInOut', [
+    transition(':enter', [   // :enter is alias to 'void => *'
+      style({ opacity: 0 }),
+      animate('500ms', style({ opacity: 1 }))
+    ]),
+    transition(':leave', [   // :leave is alias to '* => void'
+      animate('500ms', style({ opacity: 0 }))
+    ])
+  ])]
 })
 export class TopToolbarComponent implements OnInit {
 
-  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+  public route: string;
+
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer, public router: Router, public location: Location) {
+    this.route = '';
+    router.events.subscribe(() => {
+      if (location.path() !== '') {
+        this.route = location.path();
+      } else {
+        this.route = 'Home';
+      }
+    });
     iconRegistry.addSvgIcon(
       'facebook',
       sanitizer.bypassSecurityTrustResourceUrl('assets/icons/facebook.svg'));
@@ -17,8 +39,4 @@ export class TopToolbarComponent implements OnInit {
       'twitter',
       sanitizer.bypassSecurityTrustResourceUrl('assets/icons/twitter.svg'));
   }
-
-  ngOnInit() {
-  }
-
 }
