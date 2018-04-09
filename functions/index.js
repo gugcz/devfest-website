c1475eeb0955cdffb6f48536352e6124982eec7e
Dominik Šimoník <32575328+ryzizub@users.noreply.github.com>
2018-04-09 22:01:41 +0200

    hide api keys

--- functions/index.js
+++ functions/index.js
@@ -6,10 +6,10 @@ admin.initializeApp(functions.config().firebase);
 exports.recievedNewSubscriber = functions.firestore.document('mailchimp-emails/{pushId}').onCreate(function (event) {
   var newValue = event.data.data();
   var email = newValue.email;
-  return request.post('https://us4.api.mailchimp.com/3.0/lists/3a27fbd474/members', {
+  return request.post('https://us4.api.mailchimp.com/3.0/lists/' + functions.config().mailchimpIdEarlySubscribeList + '/members', {
     'auth': {
       'user': 'anystring',
-      'password': 'f02dc0c3e149f6005d6d31e252c0e535-us4'
+      'password': +functions.config().mailchimpApiKey
     },
     'json': {
       "email_address": email,
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,4 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: 'AIzaSyDtuF9K1k7-crG8_xqmM6Fi49ZFTbPxmgU',
-    authDomain: 'devfest-2018-cz.firebaseapp.com',
-    databaseURL: 'https://devfest-2018-cz.firebaseio.com',
-    projectId: 'devfest-2018-cz',
-    storageBucket: 'devfest-2018-cz.appspot.com',
-    messagingSenderId: '615143123799'
-  }
+  firebase: '../../config.json'
 };
