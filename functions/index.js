68a8db2b1b28d369d764f3c1c4d8223865b09d62
Dominik Šimoník <32575328+ryzizub@users.noreply.github.com>
2018-04-09 21:21:39 +0200

    mailchimp api cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,19 @@
 var functions = require('firebase-functions');
 var admin = require('firebase-admin');
+var request = require('request');
 admin.initializeApp(functions.config().firebase);
+
+exports.recievedNewSubscriber = functions.firestore.document('mailchimp-emails/{pushId}').onCreate(function (event) {
+  var newValue = event.data.data();
+  var email = newValue.email;
+  return request.post('https://us4.api.mailchimp.com/3.0/lists/3a27fbd474/members', {
+    'auth': {
+      'user': 'anystring',
+      'password': 'f02dc0c3e149f6005d6d31e252c0e535-us4'
+    },
+    'json': {
+      "email_address": email,
+      "status": "subscribed"
+    }
+  });
+});
