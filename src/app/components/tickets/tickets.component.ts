99c77b5cab47906e54fd256d423d04ca069e2037
Dominik Šimoník <dominik.simonik@gmail.com>
2018-06-07 00:14:16 +0200

    tests

--- src/app/components/tickets/tickets.component.ts
+++ src/app/components/tickets/tickets.component.ts
@@ -26,11 +26,8 @@ export class TicketsComponent implements OnInit {
       this.processTickets();
       this.showSpinner = false;
     });
-    const headers = new HttpHeaders();
-    headers.append('Authorization', 'Token token=wy-3g2toPzQfsY_5zsCQOw');
-    headers.append('Accept', 'application/vnd.api+json');
-    this.http.get('https://api.tito.io/v2/devfest-cz/2018/releases', {headers: headers}).toPromise().then(console.log).catch(console.log);
-
+    const headers = new HttpHeaders().set('Authorization', 'Token token=BbHzz_AxrAyMYX3sVv-E').set('Accept', 'application/vnd.api+json');
+    this.http.get('https://api.tito.io/v2/devfest-cz/2018/releases', {headers}).toPromise().then(console.log).catch(console.log);
     this.response = {
       'data': [
         {
