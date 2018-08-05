2c54916b95574454ee23e564b9fb5a2ae2152aac
Dominik Šimoník <32575328+ryzizub@users.noreply.github.com>
2018-08-05 16:02:09 +0200

    media sections

---
+++ src/app/components/media-contact/media-contact.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-media-contact',
+    templateUrl: './media-contact.component.html',
+    styleUrls: ['./media-contact.component.css']
+})
+export class MediaContactComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
