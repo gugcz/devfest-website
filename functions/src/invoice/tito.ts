66e5eed28dcf57032033d1c8fd3844c7af38e58b
Dominik Šimoník <32575328+ryzizub@users.noreply.github.com>
2018-08-29 20:44:49 +0200

    only company funded discount

--- functions/src/invoice/tito.ts
+++ functions/src/invoice/tito.ts
@@ -19,7 +19,8 @@ export async function generateTitoCode(companyName, id, countTickets: string) {
           "code": (companyName + "-" + id),
           "discount_code_type": "PercentOffDiscountCode",
           "value": "100.00",
-          "quantity": countTickets
+          "quantity": countTickets,
+          "release-ids": await getIdsOfCompanyFunded()
         }
       }
     },
@@ -30,5 +31,40 @@ export async function generateTitoCode(companyName, id, countTickets: string) {
 }
 
 export async function getActualPriceCompanyFunded() {
+  const options = {
+    method: 'GET',
+    url: 'https://api.tito.io/v2/devfest-cz/2018/releases',
+    headers: {
+      'Authorization': `Token token=${functions.config().tito.key}`,
+      'Accept': 'application/vnd.api+json',
+      'Content-Type': 'application/json'
+    }
+  };
+  const releases = JSON.parse(await rp(options));
+  const tickets = releases.data;
+  const now = new Date();
+  const companyTickets = tickets.filter(it => (it.attributes.title.indexOf('Company funded') !== -1));
+  const chosen = companyTickets.filter(it => ((it.attributes.state === 'on_sale') && (now >= new Date(it.attributes['start-at']) && now <= new Date(it.attributes['end-at']))));
+  const chosenTicket = chosen[chosen.length - 1];
+  return chosenTicket.attributes.price;
+}
 
+export async function getIdsOfCompanyFunded() {
+  const options = {
+    method: 'GET',
+    url: 'https://api.tito.io/v2/devfest-cz/2018/releases',
+    headers: {
+      'Authorization': `Token token=${functions.config().tito.key}`,
+      'Accept': 'application/vnd.api+json',
+      'Content-Type': 'application/json'
+    }
+  };
+  const releases = JSON.parse(await rp(options));
+  const tickets = releases.data;
+  const companyTickets = tickets.filter(it => (it.attributes.title.indexOf('Company funded') !== -1));
+  const ids = [];
+  companyTickets.forEach((oneTicket) => {
+    ids.push(oneTicket.id);
+  })
+  return ids;
 }
