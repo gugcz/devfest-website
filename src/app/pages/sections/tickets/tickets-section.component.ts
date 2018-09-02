ec50f08f931550ce78f34cb821a4c54d0b4dd6ff
Dominik Šimoník <32575328+ryzizub@users.noreply.github.com>
2018-09-02 16:27:04 +0200

    tickets section

---
+++ src/app/pages/sections/tickets/tickets-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tickets-section',
+  templateUrl: './tickets-section.component.html',
+  styleUrls: ['./tickets-section.component.scss']
+})
+export class TicketsSectionComponent  {
+
+}
