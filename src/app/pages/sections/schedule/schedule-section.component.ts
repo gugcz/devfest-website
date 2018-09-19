e805a19fe394da694c89a50611073f8e67591f1c
Václav Pavlíček <vaclav@pavlicek.cz>
2018-09-20 00:40:19 +0200

    Move data to talk component

--- src/app/pages/sections/schedule/schedule-section.component.ts
+++ src/app/pages/sections/schedule/schedule-section.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TeamSectionComponent} from '../team/team-section.component';
 import {MatDialogRef} from '@angular/material';
+import {Talk, Speaker} from '../../../database/talk';
 
 @Component({
   selector: 'app-schedule-section',
@@ -9,7 +10,20 @@ import {MatDialogRef} from '@angular/material';
 })
 export class ScheduleSectionComponent implements OnInit {
 
-  constructor(public dialogRef: MatDialogRef<ScheduleSectionComponent>) { }
+  talk: Talk;
+
+  constructor(public dialogRef: MatDialogRef<ScheduleSectionComponent>) {
+    const speaker: Speaker = {name: 'Ozzy Osbourne', job: 'Metal', city: 'Birmingham'};
+    this.talk = {
+      title: 'Bark at the moon',
+      speaker: speaker,
+      level: 'Beginner',
+      language: 'English',
+      length: '30 min',
+      imageUrl: 'https://www.welt.de/img/kultur/pop/mobile155156484/7982506097-ci102l-w1024/Amy-US-Premiere-Hosted-By-Lucian-Grainge-CBE-Universal-Music-Group-And-A24-4.jpg',
+      technologyClass: 'firebase'
+    };
+  }
 
   ngOnInit() {
   }
