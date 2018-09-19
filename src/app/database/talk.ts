117a67351cad557f406b8c1ca558b35c620733d5
Václav Pavlíček <vaclav@pavlicek.cz>
2018-09-20 01:17:31 +0200

    Create schedule layout system in JS

--- src/app/database/talk.ts
+++ src/app/database/talk.ts
@@ -1,15 +1,20 @@
 export interface Talk {
   title: string;
-  speaker: Speaker;
-  level: string;
-  language: string;
-  length: string;
-  imageUrl: string;
-  technologyClass: string;
+  speaker?: Speaker;
+  level?: string;
+  language?: string;
+  length?: string;
+  technologyClass?: string;
+  columnStart?: number;
+  columnEnd?: number;
+  trackNumber?: number;
+  rowStart: number;
+  rowEnd: number;
 }
 
 export interface Speaker {
   name: string;
   job: string;
   city: string;
+  imageUrl: string;
 }
