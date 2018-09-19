bb258cd844ff1c7c3317d07ac4ae437554da4b47
Václav Pavlíček <vaclav@pavlicek.cz>
2018-09-20 01:17:31 +0200

    Create schedule layout system in JS

--- src/app/pages/sections/schedule/schedule-section.component.ts
+++ src/app/pages/sections/schedule/schedule-section.component.ts
@@ -10,19 +10,16 @@ import {Talk, Speaker} from '../../../database/talk';
 })
 export class ScheduleSectionComponent implements OnInit {
 
-  talk: Talk;
+  talks: Talk[];
 
   constructor(public dialogRef: MatDialogRef<ScheduleSectionComponent>) {
-    const speaker: Speaker = {name: 'Ozzy Osbourne', job: 'Metal', city: 'Birmingham'};
-    this.talk = {
-      title: 'Bark at the moon',
-      speaker: speaker,
-      level: 'Beginner',
-      language: 'English',
-      length: '30 min',
-      imageUrl: 'https://www.welt.de/img/kultur/pop/mobile155156484/7982506097-ci102l-w1024/Amy-US-Premiere-Hosted-By-Lucian-Grainge-CBE-Universal-Music-Group-And-A24-4.jpg',
-      technologyClass: 'firebase'
-    };
+    const speaker: Speaker = {name: 'Ozzy Osbourne', job: 'Metal', city: 'Birmingham', imageUrl: 'https://www.welt.de/img/kultur/pop/mobile155156484/7982506097-ci102l-w1024/Amy-US-Premiere-Hosted-By-Lucian-Grainge-CBE-Universal-Music-Group-And-A24-4.jpg'};
+    const keynote: Talk = {title: 'Keynote', columnStart: 1, columnEnd: 3, rowStart: 1, rowEnd: 1};
+    const talk1: Talk = {title: 'Bark at the moon', rowStart: 2, rowEnd: 3, trackNumber: 1, speaker: speaker, level: 'Beginner', language: 'English', technologyClass: 'firebase'};
+    const talk2: Talk = {title: 'Bark at the moon', rowStart: 2, rowEnd: 3, trackNumber: 2, speaker: speaker, level: 'Beginner', language: 'English', technologyClass: 'android'};
+    const talk3: Talk = {title: 'Bark at the moon', rowStart: 2, rowEnd: 2, trackNumber: 3, speaker: speaker, level: 'Beginner', language: 'English', technologyClass: 'dart'};
+    const talk4: Talk = {title: 'Bark at the moon', rowStart: 3, rowEnd: 3, trackNumber: 3, speaker: speaker, level: 'Beginner', language: 'English', technologyClass: 'firebase'};
+    this.talks = [keynote, talk1, talk2, talk3, talk4];
   }
 
   ngOnInit() {
