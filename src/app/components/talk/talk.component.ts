c3cbd4892b2ab001a7b2321e3542c56bba4de0e2
Václav Pavlíček <vaclav@pavlicek.cz>
2018-09-19 23:45:14 +0200

    Move talk to custom component

---
+++ src/app/components/talk/talk.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-talk',
+  templateUrl: './talk.component.html',
+  styleUrls: ['./talk.component.scss']
+})
+export class TalkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
