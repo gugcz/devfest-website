44d246086f193e9320918330f6e4d287cae7627f
Václav Pavlíček <vaclav@pavlicek.cz>
2018-10-06 14:21:09 +0200

    Add track titles loading from Firebase

--- src/app/pages/sections/schedule/schedule-section.component.ts
+++ src/app/pages/sections/schedule/schedule-section.component.ts
@@ -17,28 +17,39 @@ export class ScheduleSectionComponent implements OnInit {
 
   timeSlots: TimeSlot[];
   selectedTimeSlotId: string;
+  timeSlotTracksRefs: {};
+  timeSlotTracks: {};
 
   constructor(public dialogRef: MatDialogRef<ScheduleSectionComponent>, private firestore: AngularFirestore) {
   }
 
   ngOnInit() {
-    this.processTimeSlots();
+    this.timeSlots = [];
+    this.selectedTimeSlotId = '';
+    this.timeSlotTracksRefs = {};
+    this.timeSlotTracks = {};
+
+    this.processTimeSlots()
+      .then(this.processTracks.bind(this));
   }
 
   async processTimeSlots() {
-    this.timeSlots = [];
-    this.selectedTimeSlotId = '';
-
     const timeSlotsSnapshot = await this.firestore.collection('timeSlots').ref.get();
     timeSlotsSnapshot.docs.forEach(timeSlotSnap => {
       const data = timeSlotSnap.data();
+      this.timeSlotTracksRefs[data.id] = [];
 
       this.selectedTimeSlotId = data.primary ? data.id : this.selectedTimeSlotId;
 
-      const timeSlotsItems: TimeSlotItem[] = data.sessions.map(timeSlotItem => ({
-        session: timeSlotItem.session,
-        track: timeSlotItem.track
-      }));
+      const timeSlotsItems: TimeSlotItem[] = data.sessions.map(timeSlotItem => {
+        if (this.timeSlotTracksRefs[data.id].indexOf(timeSlotItem.track.id) === -1) {
+          this.timeSlotTracksRefs[data.id].push(timeSlotItem.track.id);
+        }
+        return {
+            session: timeSlotItem.session,
+            track: timeSlotItem.track
+        };
+      });
 
       const timeSlot: TimeSlot = {
         id: data.id,
@@ -49,6 +60,22 @@ export class ScheduleSectionComponent implements OnInit {
       };
 
       this.timeSlots.push(timeSlot);
+    });
+  }
+
+  async processTracks() {
+    const tracksSnapshot = await this.firestore.collection('tracks').ref.get();
+
+    tracksSnapshot.docs.forEach(trackSnapshot => {
+      Object.keys(this.timeSlotTracksRefs).forEach(timeSlotRef => {
+        if (this.timeSlotTracksRefs[timeSlotRef].indexOf(trackSnapshot.ref.id) !== -1) {
+          if (!this.timeSlotTracks[timeSlotRef]) {
+            this.timeSlotTracks[timeSlotRef] = [];
+          }
+
+          this.timeSlotTracks[timeSlotRef].push(trackSnapshot.data());
+        }
+      });
     });
   }
 
