fe7273b1731ffb8f498e2fd836056723dc752bc8
Dominik Šimoník <32575328+ryzizub@users.noreply.github.com>
2018-10-07 00:03:50 +0200

    partners sync

--- functions/src/sync/index.ts
+++ functions/src/sync/index.ts
@@ -1,5 +1,13 @@
 import * as tags from './tags';
+import * as partners from './partners';
 
 export const changedTag = tags.changed;
 export const createdTag = tags.created;
 export const deletedTag = tags.deleted;
+
+export const changedSection = partners.changedSection;
+export const createdSection = partners.createdSection;
+export const deletedSection = partners.deletedSection;
+export const createdPartner = partners.createdPartner;
+export const changedPartner = partners.createdPartner;
+export const deletedPartner = partners.deletedPartner;
