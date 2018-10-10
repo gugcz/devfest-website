30a5bf0c5c5e746ac70b7b9d6ec0c126e91513c0
Dominik Šimoník <32575328+ryzizub@users.noreply.github.com>
2018-10-10 20:43:45 +0200

    change speakers sync to array

--- functions/src/sync/speakers.ts
+++ functions/src/sync/speakers.ts
@@ -1,69 +1,39 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
-interface SpeakerFirestore {
-    about: string;
-    cardPosition: number;
-    companies: string[];
-    intro: string;
-    job: string;
-    name: string;
-    photo: string;
-    show: boolean;
-    twitter?: string;
-}
-
-export const createdSpeaker = functions.firestore.document('speakers/{speakerId}').onCreate((snap, context) => {
-  const id = snap.id;
-  const speaker =  snap.data();
-  return updateOrCreateSpeaker(id, speakerDocumentToObject(speaker));
+export const changedSpeakers = functions.firestore.document('speakers/{speakerId}').onWrite((snap, context) => {
+  return updateOrCreateSpeaker();
 });
 
-export const changedSpeaker = functions.firestore.document('speakers/{speakerId}').onUpdate((snap, context) => {
-  const id = snap.after.id;
-  const speaker = snap.after.data();
-  return updateOrCreateSpeaker(id, speakerDocumentToObject(speaker));
-});
-
-export const deletedSpeaker = functions.firestore.document('speakers/{speakerId}').onDelete((snap, context) => {
-  return admin.database().ref('speakers').child(snap.id).ref.remove();
-});
-
-
-async function updateOrCreateSpeaker(id, speakerFire: SpeakerFirestore) {
-  const databaseTag = await admin.database().ref('speakers').child(id).once('value');
-  let speaker = databaseTag.val();
-  if (speaker === null) {
-    speaker = {};
+async function updateOrCreateSpeaker() {
+  const allSpeakersFirestore = await admin.firestore().collection('speakers').get();
+  const pushArray = [];
+  let count = 0;
+  for (const speakerFireDoc of allSpeakersFirestore.docs){
+    try {
+      const speaker = {};
+      const speakerFire = speakerFireDoc.data();
+      speaker["bio"] = speakerFire.about;
+      const companyArray = speakerFire.companies[0].split("/");
+      speaker["company"] = companyArray[companyArray.length-1].slice(0,-4);
+      speaker["companyLogo"] = speakerFire.companies[0];
+      speaker["featured"] = speakerFire.show;
+      speaker["id"] = count;
+      speaker["customId"] = speakerFireDoc.id;
+      speaker["name"] = speakerFire.name;
+      speaker["photoUrl"] = speakerFire.photo;
+      speaker["shortBio"] = speakerFire.about.substring(0,80) + "...";
+      speaker["socials"] = [];
+      if (speakerFire.twitter){
+        speaker["socials"][0] = {icon: "twitter", link: ("https://twitter.com/" + speaker['twitter']), name: "Twitter"};
+      }
+      speaker["tags"] = [];
+      speaker["title"] = speakerFire.job;
+      pushArray.push(speaker);
+      count += 1;
+    } catch (e){
+      count -= 1;
+    }
   }
-  speaker["bio"] = speakerFire.about;
-  const companyArray = speakerFire.companies[0].split("/");
-  speaker["company"] = companyArray[companyArray.length-1].slice(0,-4);
-  speaker["companyLogo"] = speakerFire.companies[0];
-  speaker["featured"] = speakerFire.show;
-  speaker["id"] = id;
-  speaker["name"] = speakerFire.name;
-  speaker["photoUrl"] = speakerFire.photo;
-  speaker["shortBio"] = speakerFire.about.substring(0,80) + "...";
-  speaker["socials"] = [];
-  if (speakerFire.twitter){
-    speaker["socials"][0] = {icon: "twitter", link: ("https://twitter.com/" + speaker.twitter), name: "Twitter"};
-  }
-  speaker["tags"] = [];
-  speaker["title"] = speakerFire.job;
-  return admin.database().ref('speakers').child(id).update(speaker);
-}
-
-function speakerDocumentToObject(document): SpeakerFirestore {
-  return {
-    about: document.about,
-    cardPosition: document.cardPosition,
-    companies: document.companies,
-    intro: document.intro,
-    job: document.job,
-    name: document.name,
-    photo: document.photo,
-    show: document.show,
-    twitter: document.twitter? document.twitter: undefined
-  }
+  return admin.database().ref('speakers').set(pushArray);
 }
