68f8aaeff6e433b81777b5d4bca4481039cd7501
Dominik Šimoník <32575328+ryzizub@users.noreply.github.com>
2018-10-10 21:03:35 +0200

    change partners sync to array

--- functions/src/sync/index.ts
+++ functions/src/sync/index.ts
@@ -8,12 +8,8 @@ export const changedTag = tags.changed;
 export const createdTag = tags.created;
 export const deletedTag = tags.deleted;
 
-export const changedSection = partners.changedSection;
-export const createdSection = partners.createdSection;
-export const deletedSection = partners.deletedSection;
-export const createdPartner = partners.createdPartner;
-export const changedPartner = partners.changedPartner;
-export const deletedPartner = partners.deletedPartner;
+export const changedSections = partners.changedSections;
+export const changedPartners = partners.changedPartners;
 
 export const changedSpeakers = speakers.changedSpeakers;
 
