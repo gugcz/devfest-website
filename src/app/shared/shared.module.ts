5d7717a46a896f0b4f42377ee76c36aa816c3471
Dominik Šimoník <32575328+ryzizub@users.noreply.github.com>
2018-12-17 20:41:26 +0100

    home page

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,18 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NeonLogoComponent } from './neon-logo/neon-logo.component';
 import { NeonDateComponent } from './neon-date/neon-date.component';
+import { FooterComponent } from './footer/footer.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   imports: [
     CommonModule,
+    MatIconModule,
+    MatButtonModule
   ],
   declarations: [
     NeonLogoComponent,
     NeonDateComponent,
+    FooterComponent,
   ],
   exports: [
     NeonLogoComponent,
-    NeonDateComponent
+    NeonDateComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
