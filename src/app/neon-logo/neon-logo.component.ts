4bea15c154d0c4f7532e0ad79ff045efaf2fe3c1
Dominik Šimoník <32575328+ryzizub@users.noreply.github.com>
2018-12-17 10:56:32 +0100

    neon-logo

---
+++ src/app/neon-logo/neon-logo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-neon-logo',
+  templateUrl: './neon-logo.component.html',
+  styleUrls: ['./neon-logo.component.css']
+})
+export class NeonLogoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
