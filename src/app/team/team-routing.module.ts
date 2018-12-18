a2b4fb9605fa7a2e4031311328002a7158ddd8d0
Dominik Šimoník <32575328+ryzizub@users.noreply.github.com>
2018-12-18 17:03:29 +0100

    [new] create team module

---
+++ src/app/team/team-routing.module.ts
@@ -0,0 +1,16 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { TeamComponent } from './team.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TeamComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TeamRoutingModule { }
