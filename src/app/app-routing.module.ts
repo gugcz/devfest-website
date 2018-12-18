614e9052739d9b5f2345346cf89435487bef198e
Dominik Šimoník <32575328+ryzizub@users.noreply.github.com>
2018-12-18 16:56:29 +0100

    [new] - navigation component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,10 @@ import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     loadChildren:  './home/home.module#HomeModule'
   },
-  {
-    path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
-  },
-  { path: '**', redirectTo: '/home' }
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
