f2598f56b520e895577f5e43f341a96f53e158ae
ryzizub <dominik.simonik@gmail.com>
2018-12-26 12:28:08 +0100

    [new] add firebase to project

--- src/app/team/team.module.ts
+++ src/app/team/team.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TeamComponent } from './team.component';
 import { TeamRoutingModule } from './team-routing.module';
+import { MemberCardComponent } from './member-card/member-card.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    TeamRoutingModule
+    TeamRoutingModule,
+    SharedModule,
   ],
-  declarations: [TeamComponent]
+  declarations: [TeamComponent, MemberCardComponent]
 })
 export class TeamModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDZYJFycJ-hQUvhHj5IQrSNaZpPz2KeBmA",
+    authDomain: "devfest-2019-1f996.firebaseapp.com",
+    databaseURL: "https://devfest-2019-1f996.firebaseio.com",
+    projectId: "devfest-2019-1f996",
+    storageBucket: "devfest-2019-1f996.appspot.com",
+    messagingSenderId: "24797806338"
+  }
 };
 
 /*
