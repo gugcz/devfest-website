d5d42afb006b4836df4930d8c1864f9e14feb97f
Dominik Šimoník <dominik.simonik@gmail.com>
2019-02-09 20:02:55 +0100

    update database of partners

--- src/app/dto/partner.ts
+++ src/app/dto/partner.ts
@@ -1,5 +1,12 @@
 export interface Partner {
     name: string;
+    order: number;
+    top: boolean;
+    companies: Company[];
+}
+
+interface Company {
+    name: string;
     url: string;
     photoPath: string;
 }
