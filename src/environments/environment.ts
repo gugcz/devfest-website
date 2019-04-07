09bc051b3e6d2990877516a2c8844abdb049a840
Dominik Simonik <dominik.simonik@gmail.com>
2019-04-07 22:54:07 +0200

    delete firebase server settings

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,12 +6,12 @@ export const environment = {
   production: false,
   tickets: false,
   firebase: {
-    apiKey: "AIzaSyC0fQ_CoXIIP-BO-Y72zg_yqdht-n_5a24",
-    authDomain: "devfestcz-test.firebaseapp.com",
-    databaseURL: "https://devfestcz-test.firebaseio.com",
-    projectId: "devfestcz-test",
-    storageBucket: "devfestcz-test.appspot.com",
-    messagingSenderId: "425339957531"
+    apiKey: 'AIzaSyC0fQ_CoXIIP-BO-Y72zg_yqdht-n_5a24',
+    authDomain: 'devfestcz-test.firebaseapp.com',
+    databaseURL: 'https://devfestcz-test.firebaseio.com',
+    projectId: 'devfestcz-test',
+    storageBucket: 'devfestcz-test.appspot.com',
+    messagingSenderId: '425339957531'
   }
 };
 
