58a5ed002c4de7b13b1f3fe3cb5193da3f03d5eb
ryzizub <dominik.simonik@gmail.com>
2019-05-12 22:06:14 +0200

    image panel

---
+++ src/app/home/photo-panel/photo-panel.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-photo-panel',
+  templateUrl: './photo-panel.component.html',
+  styleUrls: ['./photo-panel.component.scss']
+})
+export class PhotoPanelComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
