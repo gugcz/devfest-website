7dc3bc94e074f6061b3b50ec954a9f22a48841cf
ryzizub <dominik.simonik@gmail.com>
2019-07-13 20:02:07 +0200

    email and event config links

--- src/app/core/faq/faq.component.ts
+++ src/app/core/faq/faq.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AngularFirestore} from '@angular/fire/firestore';
 import {Observable} from 'rxjs';
+import config from 'src/config';
 
 @Component({
   selector: 'app-faq',
@@ -10,6 +11,9 @@ import {Observable} from 'rxjs';
 export class FaqComponent implements OnInit {
 
   public questions: Observable<FAQ[]>;
+  public facebookEvent = config.facebookEvent;
+  public meetupEvent = config.meetupEvent;
+  public email = config.email;
 
   constructor(private angularfirestore: AngularFirestore) {
   }
--- src/config.ts
+++ src/config.ts
@@ -5,4 +5,9 @@ export default {
     twitter: 'https://twitter.com/devfest_cz',
     facebook: 'https://www.facebook.com/DevFestCZ',
     instagram: 'https://www.instagram.com/devfestcz/',
+    github: 'https://github.com/gugcz/devfest-website',
+    facebookEvent: 'https://www.facebook.com/events/752518098447370/',
+    meetupEvent: 'https://www.meetup.com/GDGPrague/events/257340706/',
+    email: 'devfest@gug.cz',
+    partnersEmail: ''
 };
