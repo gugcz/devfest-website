0782894aab0af1d1ea2944f251557cfb556de575
ryzizub <dominik.simonik@gmail.com>
2019-07-14 11:38:25 +0200

    Tag color border

--- src/app/schedule/components/tag-avatar/tag-avatar.component.ts
+++ src/app/schedule/components/tag-avatar/tag-avatar.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, AfterViewInit } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-tag-avatar',
   templateUrl: './tag-avatar.component.html',
   styleUrls: ['./tag-avatar.component.scss']
 })
-export class TagAvatarComponent implements OnInit {
+export class TagAvatarComponent implements AfterViewInit {
 
-  constructor() { }
+  @Input() firestorageImagePath = 'speakers/no-image.jpg';
+  @Input() tagColor = '#fff';
+  public imageUrl: Observable<string>;
 
-  ngOnInit() {
+  constructor(private fireStorage: AngularFireStorage) { }
+
+  ngAfterViewInit() {
+    this.imageUrl = this.fireStorage.ref(this.firestorageImagePath).getDownloadURL();
   }
 
 }
