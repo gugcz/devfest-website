5cfd8dc3a53475b1fd40c5b3d88a4b7757b66c71
Václav Pavlíček <venda.pavlicek@gmail.com>
2019-09-04 15:53:28 +0200

    Update schedule after speaker update.

--- functions/src/schedule/speakers.ts
+++ functions/src/schedule/speakers.ts
@@ -9,4 +9,25 @@ export const buildSpeakerForSchedule = speakerData => pipe(
     assoc('company', propOr('', 'company', speakerData)),
     assoc('tag', propOr('', 'tag', speakerData)),
     assoc('tagColor', propOr('', 'tagColor', speakerData)),
+    assoc('pronoun', propOr('', 'pronoun', speakerData)),
 )({});
+
+export const onSpeakerUpdate = functions.firestore.document('speakers/{documentId}').onUpdate((change, context) => {
+    return admin.firestore().collection('rooms').get().then(snapshot => {
+        snapshot.forEach(doc => {
+            const data = doc.data();
+            if (data.schedule && data.schedule.length > 0) {
+                data.schedule = data.schedule.map(scheduleItem => {
+                    if (scheduleItem.speakerRef && scheduleItem.speakerRef.path === change.after.ref.path) {
+                        scheduleItem.speaker = buildSpeakerForSchedule(change.after.data());
+                    }
+                    if (scheduleItem.cospeakerRef && scheduleItem.cospeakerRef.path === change.after.ref.path) {
+                        scheduleItem.cospeaker = buildSpeakerForSchedule(change.after.data());
+                    }
+                    return scheduleItem;
+                });
+                doc.ref.set(data);
+            }    
+        });
+    });
+});
