320c575e409298ad6558d7469e72ad0371761c20
Dominik Šimoník <dominik.simonik@gmail.com>
2019-09-30 22:26:21 +0200

    fix

--- src/app/components/member-panel-avatar/member-panel-avatar.module.ts
+++ src/app/components/member-panel-avatar/member-panel-avatar.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MemberPanelAvatarComponent } from './member-panel-avatar.component';
-import { MemberPanelComponent } from '../member-panel/member-panel.component';
 import { SocialIconsService } from 'src/app/services/social-icons.service';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [MemberPanelAvatarComponent],
-  exports: [MemberPanelComponent],
+  exports: [MemberPanelAvatarComponent],
   providers: [SocialIconsService],
   imports: [AngularFireStorageModule, CommonModule],
 })
