920a8d674a843641fb09fe3aa64d0e719231e84e
Dominik Šimoník <dominik.simonik@gmail.com>
2019-10-06 15:50:13 +0200

    schedule block click

--- src/app/components/schedule-block/schedule-block.component.ts
+++ src/app/components/schedule-block/schedule-block.component.ts
@@ -1,10 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import Speaker from 'src/app/data/speaker';
+import { animFadeInOut } from 'src/app/animations';
 
 @Component({
   selector: 'app-schedule-block',
   templateUrl: './schedule-block.component.html',
   styleUrls: ['./schedule-block.component.scss'],
+  animations: [
+    animFadeInOut
+  ]
 })
 export class ScheduleBlockComponent {
 
@@ -14,6 +18,7 @@ export class ScheduleBlockComponent {
   @Input() duration: number;
   @Input() speaker1: Speaker;
   @Input() speaker2: Speaker;
+  @Output() blockClicked = new EventEmitter<void>();
 
   constructor() {}
 }
