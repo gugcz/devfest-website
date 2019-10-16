e9dabeb26eff15d713aa9bd42ce1b6d4277689a2
Dominik Šimoník <32575328+ryzizub@users.noreply.github.com>
2019-10-16 19:17:33 +0200

    Fakturoid move invoices (#72)
    * move sending invoice to new function
    
    * update index

--- functions/src/tickets/helpers.ts
+++ functions/src/tickets/helpers.ts
@@ -27,7 +27,7 @@ export async function sendInfoIntoSlack(message): Promise<Object> {
     headers: {
       'Content-Type': 'application/json',
     },
-    body: message,
+    body: JSON.parse(message),
     json: true
   };
   return await rp(options);
--- functions/src/tickets/invoices.ts
+++ functions/src/tickets/invoices.ts
@@ -9,80 +9,109 @@ import * as tito from './tito';
 /**
  * When request for new invoice is received
  */
-export const newInvoiceRequest = functions.firestore.document('invoiceRequests/{requestId}')
-    .onCreate(async (snap, context) => {
-        const id = context.params.invoiceId;
-        const newValue = snap.data();
-        const email = newValue.email;
-        const companyName = newValue.companyName;
-        const street = newValue.street;
-        const city = newValue.city;
-        const zip = newValue.zip;
-        const registrationNumberIC = newValue.registrationNumberIC;
-        const vatNumber = (newValue.vatNumber ? newValue.vatNumber : null);
-        const country = newValue.country;
-        const countNormal = newValue.countTicketsNormal;
-        const countVIP = newValue.countTicketsVIP;
-        await informIntoSlackAboutNewInvoice(companyName, countNormal, countVIP);
-        let invoiceCompanyId = await fakturoid.findFaktruoidCompanyId(companyName);
-        if (invoiceCompanyId === null) {
-            const company = {
-                firebaseId: id,
-                name: companyName,
-                street: street,
-                city: city,
-                email: email,
-                zip: zip,
-                registrationNumberIC: registrationNumberIC,
-                country: country
-            };
-            if (vatNumber) {
-                company['vatNumber'] = vatNumber;
-            }
-            invoiceCompanyId = await fakturoid.createFakturoidCompany(company);
-        }
-        const invoice = await fakturoid.createInvoice(invoiceCompanyId, countNormal, countVIP);
-        await snap.ref.update({
-            invoiceFound: true,
-            invoiceId: invoice.id,
-            invoiceVariable: invoice.variableSymbol
-        });
-        const downloaded = await fakturoid.downloadInvoiceById(invoice.id);
-        const mail = await sendgrid.sendInvoiceInEmail(downloaded, email);
-        if (mail) {
-            await snap.ref.update({
-                invoiceSended: true
-            })
-        } else {
-            throw Error('something went wrong with sending invoice');
-        }
-        return helpers.sendInfoIntoSlack({
-            "text": "Faktura zaslána na email :email:"
-        });
+export const newInvoiceRequest = functions.firestore
+  .document('invoiceRequests/{requestId}')
+  .onCreate(async (snap, context) => {
+    const id = context.params.invoiceId;
+    const newValue = snap.data();
+    const email = newValue.email;
+    const companyName = newValue.companyName;
+    const street = newValue.street;
+    const city = newValue.city;
+    const zip = newValue.zip;
+    const registrationNumberIC = newValue.registrationNumberIC;
+    const vatNumber = newValue.vatNumber ? newValue.vatNumber : null;
+    const country = newValue.country;
+    const countNormal = newValue.countTicketsNormal;
+    const countVIP = newValue.countTicketsVIP;
+    await informIntoSlackAboutNewInvoice(companyName, countNormal, countVIP);
+    let invoiceCompanyId = await fakturoid.findFaktruoidCompanyId(companyName);
+    if (invoiceCompanyId === null) {
+      const company = {
+        firebaseId: id,
+        name: companyName,
+        street: street,
+        city: city,
+        email: email,
+        zip: zip,
+        registrationNumberIC: registrationNumberIC,
+        country: country,
+      };
+      if (vatNumber) {
+        company['vatNumber'] = vatNumber;
+      }
+      invoiceCompanyId = await fakturoid.createFakturoidCompany(company);
+    }
+    const invoice = await fakturoid.createInvoice(
+      invoiceCompanyId,
+      countNormal,
+      countVIP
+    );
+    await snap.ref.update({
+      invoiceFound: true,
+      invoiceId: invoice.id,
+      invoiceVariable: invoice.variableSymbol,
+      invoiceSended: false,
     });
+    return helpers.sendInfoIntoSlack({
+      text: 'Faktura vygenerována',
+    });
+  });
+
+export const newInvoiceSendInvoice = functions.firestore
+  .document('invoiceRequests/{requestId}')
+  .onUpdate(async (change, context) => {
+    if (
+      change.after.data().invoiceFound === true &&
+      change.after.data().invoiceSended === false
+    ) {
+      const downloaded = await fakturoid.downloadInvoiceById(
+        change.after.data().invoiceId
+      );
+      const mail = await sendgrid.sendInvoiceInEmail(
+        downloaded,
+        change.after.data().email
+      );
+      if (mail) {
+        await change.after.ref.update({
+          invoiceSended: true,
+        });
+        return helpers.sendInfoIntoSlack({
+          text: 'Faktura zaslána na email :email:',
+        });
+      } else {
+        throw Error('something went wrong with sending invoice');
+      }
+    } else {
+      return true;
+    }
+  });
 
 /**
  * Setting invoice in firestore paid
  */
 export const invoicePaid = functions.https.onRequest((req, res) => {
-    const body = req.body;
-    const key = req.query.key;
-    if (key !== functions.config().fakturoid.key) {
-        return res.status(403).send('Forbidden');
+  const body = req.body;
+  const key = req.query.key;
+  if (key !== functions.config().fakturoid.key) {
+    return res.status(403).send('Forbidden');
+  } else {
+    const invoiceId = body.invoice_id;
+    if (body.status === 'paid' && body.event_name === 'invoice_paid') {
+      return sendDiscountCodeByInvoiceId(invoiceId)
+        .then(() => {
+          return helpers.sendInfoIntoSlack({
+            text:
+              'Faktura zaplacena, slevový kupón vygenerován a zaslán na email :moneybag:',
+          });
+        })
+        .then(() => {
+          return res.send(200);
+        });
     } else {
-        const invoiceId = body.invoice_id;
-        if (body.status === "paid" && body.event_name === "invoice_paid") {
-            return sendDiscountCodeByInvoiceId(invoiceId).then(() => {
-                return helpers.sendInfoIntoSlack({
-                    "text": "Faktura zaplacena, slevový kupón vygenerován a zaslán na email :moneybag:"
-                });
-            }).then(() => {
-                return res.send(200);
-            })
-        } else {
-            return res.send(200);
-        }
+      return res.send(200);
     }
+  }
 });
 
 /**
@@ -90,57 +119,75 @@ export const invoicePaid = functions.https.onRequest((req, res) => {
  * @param invoiceId - id of invoice in fakturoid
  */
 async function sendDiscountCodeByInvoiceId(invoiceId): Promise<Object> {
-    const snapshot = await admin.firestore().collection('invoiceRequests').where('invoiceId', '==', invoiceId).get();
-    if (snapshot.empty) {
-        return false;
-    } else {
-        const info = snapshot.docs[0].data();
-        const code = await tito.generateTitoCode(info.companyName, invoiceId, (info.countTicketsNormal === null ? 0 : info.countTicketsNormal), (info.countTicketsVIP === null ? 0 : info.countTicketsVIP));
-        await admin.firestore().collection('invoiceRequests').doc(snapshot.docs[0].id).update({
-            discoundCodeSended: true,
-        });
-        return sendgrid.sendDiscountCode(code, ('https://ti.to/devfest-cz/' + new Date().getFullYear() + '/discount/' + querystring.escape(code)), info.email);
-    }
-};
+  const snapshot = await admin
+    .firestore()
+    .collection('invoiceRequests')
+    .where('invoiceId', '==', invoiceId)
+    .get();
+  if (snapshot.empty) {
+    return false;
+  } else {
+    const info = snapshot.docs[0].data();
+    const code = await tito.generateTitoCode(
+      info.companyName,
+      invoiceId,
+      info.countTicketsNormal === null ? 0 : info.countTicketsNormal,
+      info.countTicketsVIP === null ? 0 : info.countTicketsVIP
+    );
+    await admin
+      .firestore()
+      .collection('invoiceRequests')
+      .doc(snapshot.docs[0].id)
+      .update({
+        discoundCodeSended: true,
+      });
+    return sendgrid.sendDiscountCode(
+      code,
+      'https://ti.to/devfest-cz/' +
+        new Date().getFullYear() +
+        '/discount/' +
+        querystring.escape(code),
+      info.email
+    );
+  }
+}
 
 /**
  * Sends information into slack about new invoice
- * @param company 
- * @param countNormal 
- * @param countVIP 
+ * @param company
+ * @param countNormal
+ * @param countVIP
  */
-async function informIntoSlackAboutNewInvoice(company: string, countNormal: number, countVIP: number) {
-    const fields = [];
-    fields.push(
-        {
-            "title": "Jméno firmy",
-            "value": company
-        }
-    );
-    if (countNormal !== undefined && countNormal !== null) {
-        fields.push(
-            {
-                "title": "Počet lístků",
-                "value": countNormal
-            }
-        );
-    }
-    if (countVIP !== undefined && countVIP !== null) {
-        fields.push(
-            {
-                "title": "Počet lístků VIP",
-                "value": countVIP
-            }
-        );
-    }
-    const slackMessage = {
-        "text": "Žádost o novou fakturu :pencil: ",
-        "attachments": [
-            {
-                "fields": fields,
-                "color": "#7da453"
-            }
-        ]
-    }
-    return await helpers.sendInfoIntoSlack(slackMessage);
+async function informIntoSlackAboutNewInvoice(
+  company: string,
+  countNormal: number,
+  countVIP: number
+) {
+  const fields = [];
+  fields.push({
+    title: 'Jméno firmy',
+    value: company,
+  });
+  if (countNormal !== undefined && countNormal !== null) {
+    fields.push({
+      title: 'Počet lístků',
+      value: countNormal,
+    });
+  }
+  if (countVIP !== undefined && countVIP !== null) {
+    fields.push({
+      title: 'Počet lístků VIP',
+      value: countVIP,
+    });
+  }
+  const slackMessage = {
+    text: 'Žádost o novou fakturu :pencil: ',
+    attachments: [
+      {
+        fields: fields,
+        color: '#7da453',
+      },
+    ],
+  };
+  return await helpers.sendInfoIntoSlack(slackMessage);
 }
