04a4ff4eabe6e345dd68f76427eb5f61930129e6
Dominik Šimoník <dominik.simonik@gmail.com>
2019-10-23 23:18:07 +0200

    venue transport

--- src/app/components/venue-info-block/venue-info-block.module.ts
+++ src/app/components/venue-info-block/venue-info-block.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { VenueInfoBlockComponent } from './venue-info-block.component';
+import { MatButtonModule } from '@angular/material';
 
 @NgModule({
   declarations: [VenueInfoBlockComponent],
+  imports: [MatButtonModule],
   exports: [VenueInfoBlockComponent],
 })
 export class VenueInfoBlockModule {}
