ffcae6bf5d75443dc52be921ee6280250a42a956
Dominik Šimoník <dominik.simonik@gmail.com>
2019-11-03 09:18:54 +0100

    fix number badge

--- functions/src/game/login.ts
+++ functions/src/game/login.ts
@@ -6,9 +6,10 @@ import { TokenResponse, ErrorResponse } from './responses';
 const db = admin.firestore();
 
 export const deleteLogin = functions.firestore.document('users/{userId}').onDelete(async (snap, cont) => {
+  const numberBadge = snap.data().number;
   const uid = cont.params.userId;
   await admin.auth().deleteUser(uid);
-  return db.collection('numbersOnBadges').doc(uid).update({
+  return db.collection('numbersOnBadges').doc(numberBadge).update({
     isUsed: false
   });
 });
