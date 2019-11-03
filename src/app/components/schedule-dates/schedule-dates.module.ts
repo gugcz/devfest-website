5c2ae8bb60408323397d4113bdb6d178284a4f93
Dominik Šimoník <dominik.simonik@gmail.com>
2019-11-03 08:52:52 +0100

    dates

--- src/app/components/schedule-dates/schedule-dates.module.ts
+++ src/app/components/schedule-dates/schedule-dates.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { ScheduleDatesComponent } from './schedule-dates.component';
 import { MatTabsModule, MatButtonModule, MatToolbarModule } from '@angular/material';
 import { CommonModule } from '@angular/common';
+import { DeviceDetectorModule } from 'ngx-device-detector';
 
 @NgModule({
   declarations: [ScheduleDatesComponent],
   exports: [ScheduleDatesComponent],
   imports: [
       MatTabsModule,
+      DeviceDetectorModule,
       CommonModule,
       MatButtonModule,
       MatToolbarModule,
